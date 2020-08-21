4a1f14cc13e450e3f58c4315cefdc17944a01964
Simaranjit <ss@joinpiggy.com>
2020-08-22 02:44:38 +0530

    Added index

---
+++ websites/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Welcome to DKR</title>
+</head>
+
+<body>
+
+  <h1>Welcome to DKR</h1> 
+  <h2>Package Includes:</h2>
+
+  <p>Nginx</p>
+  <p>PHP 7.2</p>
+  <p>Mysql 5.7.22</p>
+  <p>PhpMyadmin</p>
+
+  <h2>Working Directory:</h2>
+
+  <p>./websites/</p>
+
+  <h2>MySQL Credentials:</h2>
+
+  <p>host: db</p>
+  <p>user: root</p>
+  <p>password: password</p>
+
+  <h2>Access PhpMyadmin:</h2>
+
+  <a href="http://localhost:8081" target="_blank">PhpMyadmin</a> (http://localhost:8081)
+
+  <h2>Where to put PHP files:</h2>
+
+  <p>Create your folder/files under ./websites directory, like ./websites/mywebsite/index.php and access that using http://localhost/mywebsite/index.php</p>
+
+  <h2>Access PHP.ini:</h2>
+
+  <p>./php/local.ini</p>
+</body>
+
+</html> 
